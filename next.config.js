fcde1444cb46e93b57d4fcb4ecef716f11014b7b
Usman Kiyani <usman.kiyani@ceative.co.uk>
2023-01-18 13:47:32 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  output: 'standalone',
+  env: {
+    REACT_APP_API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+  },
+  NODE_PATH: '.',
 }
 
 module.exports = nextConfig
---
+++ pages/test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = () => {
+  return <div>Test 158</div>;
+};
+
+export default Test;
